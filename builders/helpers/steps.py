e3ab9e9d043042f4f1a8673ad0ebdb468e10db59
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-08-26 21:16:11 +0200

    path fix

--- builders/helpers/steps.py
+++ builders/helpers/steps.py
@@ -3,7 +3,7 @@ from buildbot.plugins import steps
 def venv_step(worker: str, builder: str, requirements: str='requirements.txt'):
     return steps.ShellCommand(
         command=[
-            "python3", "/home/buildbot/scripts/load_venv.py", worker, builder,
+            "python3", "/var/buildbot/scripts/load_venv.py", worker, builder,
             "-r", requirements
         ],
         haltOnFailure=True,
