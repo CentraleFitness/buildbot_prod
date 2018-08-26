07a2e8e7a6c520c1cbdf2b139d7080a460694783
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-08-26 20:23:59 +0200

    site vitrine test

--- builders/__init__.py
+++ builders/__init__.py
@@ -1,6 +1,8 @@
 from .backoffice_api import backoffice_api_builder
+from .site_vitrine import site_vitrine_builder
 
 builders_config = [
     backoffice_api_builder,
+    site_vitrine_builder,
     # Add new builders here
 ]
---
+++ builders/site_vitrine.py
@@ -0,0 +1,16 @@
+from buildbot.plugins import util, steps
+
+
+site_vitrine_factory = util.BuildFactory([
+    steps.Git(repourl='https://github.com/CentraleFitness/site_vitrine.git',
+              mode='incremental'),
+    steps.ShellCommand(command=["yarn"]),
+    steps.ShellCommand(command=["yarn", "build"]),
+    steps.ShellCommand(command=["yarn", "start"]),
+])
+
+site_vitrine_builder = util.BuilderConfig(
+    name='SiteVitrine',
+    workername='site_vitrine',
+    factory=site_vitrine_factory
+)
