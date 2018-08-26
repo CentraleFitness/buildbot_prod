c9ad6224739e28bbf5291930d94c038358196e3c
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-08-26 21:21:50 +0200

    indent fix

--- scripts/utils/exec.py
+++ scripts/utils/exec.py
@@ -3,17 +3,17 @@ import subprocess
 def exec_cmd(cmd, timeout: int=60) -> dict:
     """Execute a given command in the shell"""
     ret = {"code": 0}
-        try:
-            ret["out"] = subprocess.check_output(
-                cmd,
-                shell=True,
-                universal_newlines=True,
-                stderr=subprocess.STDOUT,
-                timeout=timeout)
-        except subprocess.CalledProcessError as exc:
-            ret["code"] = exc.returncode
-            ret["out"] = str(exc.output)
-        except Exception as exc:
-            ret["code"] = 1
-            ret["out"] = str(exc)
+    try:
+        ret["out"] = subprocess.check_output(
+            cmd,
+            shell=True,
+            universal_newlines=True,
+            stderr=subprocess.STDOUT,
+            timeout=timeout)
+    except subprocess.CalledProcessError as exc:
+        ret["code"] = exc.returncode
+        ret["out"] = str(exc.output)
+    except Exception as exc:
+        ret["code"] = 1
+        ret["out"] = str(exc)
     return ret
