f2df7c88ba952518a656ddaa508474614ed1994a
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-08-26 21:30:09 +0200

    DAT PATH

--- builders/backoffice_api.py
+++ builders/backoffice_api.py
@@ -2,7 +2,6 @@ from buildbot.plugins import util, steps
 
 from .helpers.steps import venv_step, uwsgi_step
 
-WORKDIR = "./emailproj"
 UWSGI_DIR = "/var/buildbot/workers/backoffice_api/BackofficeApi/build/emailproj/uwsgi"
 PYTHON_EX = "/var/buildbot/workers/backoffice_api/venv/bin/python3"
 
@@ -12,7 +11,7 @@ backoffice_api_factory = util.BuildFactory([
               mode='incremental'),
     venv_step('backoffice_api', 'BackofficeApi'),
     steps.ShellCommand(
-        command=[PYTHON_EX, "manage.py", "migrate"], workdir=WORKDIR),
+        command=[PYTHON_EX, "emailproj/manage.py", "migrate"]),
     uwsgi_step('backoffice_api', UWSGI_DIR)
 ])
 
