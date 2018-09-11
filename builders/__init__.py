c5cb41b60c362366f6948c01bf6f1b4ed61f33f1
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-09-11 19:15:51 +0200

    alt builder for site vitrine

--- builders/__init__.py
+++ builders/__init__.py
@@ -1,8 +1,9 @@
 from .backoffice_api import backoffice_api_builder
-from .site_vitrine import site_vitrine_builder
+from .site_vitrine import site_vitrine_builder, site_vitrine_builder_alt
 
 builders_config = [
     backoffice_api_builder,
     site_vitrine_builder,
+    site_vitrine_builder_alt
     # Add new builders here
 ]
--- builders/site_vitrine.py
+++ builders/site_vitrine.py
@@ -1,17 +1,28 @@
 from buildbot.plugins import util, steps
 
 
-site_vitrine_factory = util.BuildFactory([
-    steps.Git(repourl='git@github.com:CentraleFitness/site_vitrine.git',
-              mode='incremental'),
-    steps.ShellCommand(command=["yarn"]),
-    steps.ShellCommand(command=["yarn", "build"]),
-    steps.ShellCommand(command=["forever", "stop", "bin/www"]),
-    steps.ShellCommand(command=["forever", "start", "bin/www"]),
-])
-
 site_vitrine_builder = util.BuilderConfig(
     name='SiteVitrine',
     workername='site_vitrine',
-    factory=site_vitrine_factory
+    factory=util.BuildFactory([
+        steps.Git(repourl='git@github.com:CentraleFitness/site_vitrine.git',
+                mode='incremental'),
+        steps.ShellCommand(command=["yarn"]),
+        steps.ShellCommand(command=["yarn", "build"]),
+        steps.ShellCommand(command=["forever", "stop", "bin/www"]),
+        steps.ShellCommand(command=["forever", "start", "bin/www"]),
+    ])
+)
+
+site_vitrine_builder_alt = util.BuilderConfig(
+    name='SiteVitrine_npm',
+    workername='site_vitrine',
+    factory=util.BuildFactory([
+        steps.Git(repourl='git@github.com:CentraleFitness/site_vitrine.git',
+                mode='incremental'),
+        steps.ShellCommand(command=["npm", "install"]),
+        steps.ShellCommand(command=["npm", "run", "build"]),
+        steps.ShellCommand(command=["forever", "stop", "bin/www"]),
+        steps.ShellCommand(command=["forever", "start", "bin/www"]),
+    ])
 )
